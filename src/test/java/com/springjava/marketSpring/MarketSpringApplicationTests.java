3964d81bd83c0e2e04b0ccdffdffe90aecd2cc69
AndresAmb <andy200229@gmail.com>
2022-12-06 11:55:56 -0600

    <First commit>

---
+++ src/test/java/com/springjava/marketSpring/MarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjava.marketSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
